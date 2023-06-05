a71391bdbdf31a2d360107e65f4c6000f9e314fc
ADK <agus.darma@emobile.com.sg>
2023-06-05 14:20:24 +0700

    update message aja

--- resources/lang/id/messages.php
+++ resources/lang/id/messages.php
@@ -6,10 +6,15 @@ return [
 
   
 
-    // landing.blade.php
-    'landingText1' => 'Diskon 10% untuk penyewa pertama',
-    'landingText2' => 'Rahasia trading otomatis bukan hanya menguntungkan saja,',
-    'landingText3' => 'tetapi konsisten. ',
+   // landing.blade.php
+   'landingText1' => 'Get 10% off on first rent',
+   'landingText2' => 'The secret is to auto-trade with a robot that is not only profitable,',
+   'landingText3' => 'but also stable. ',
+   
+   // verify-email.blade.php
+   'verifyEmailText1' => 'Verify your Email Account',
+   'verifyEmailText2' => "Before continuing, could you verify your email address by clicking on the link we just emailed to you? If you didn't receive the email, we will gladly send you another.",
+   'verifyEmailText3' => "Resend Verification Email",
   
 
 ];
